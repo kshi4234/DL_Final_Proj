780907fc4adbf8b792685362184423601706499c
Winston Zhang <midas0231@gmail.com>
2024-12-14 03:42:37 -0500

    fix loss

--- main.py
+++ main.py
@@ -183,7 +183,7 @@ def train_model(device):
             z2 = targets
             bt_loss = barlow_twins_loss(z1, z2)
 
-            total_loss_batch = 0.6 * jepa_loss + 0.4 * bt_loss
+            total_loss_batch = 0.6 * jepa_loss + 0.4 * (bt_loss / 200)
             optimizer.zero_grad()
             total_loss_batch.backward()
             optimizer.step()
